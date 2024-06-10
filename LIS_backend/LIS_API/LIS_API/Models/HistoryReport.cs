3e8c667c81b4fc2be9364b263b9ce9bce3bab656
Toretto00 <phanchibao007@gmail.com>
2024-06-10 16:34:05 +0700

    first build

--- LIS_backend/LIS_API/LIS_API/Models/HistoryReport.cs
+++ LIS_backend/LIS_API/LIS_API/Models/HistoryReport.cs
@@ -2,5 +2,9 @@
 {
     public class HistoryReport
     {
+        public int id {  get; set; }
+        public Product name { get; set; }
+        public int quantity { get; set; }
+        public int unit { get; set; }
     }
 }
