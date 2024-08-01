ce1fca6d0fdb45543b685dd3ba4aa6d341d67fdd
Toretto00 <phanchibao007@gmail.com>
2024-08-01 11:37:51 +0700

    docker compose

--- LIS_website/src/libs/auth.ts
+++ LIS_website/src/libs/auth.ts
@@ -33,7 +33,7 @@ export const authOptions: NextAuthOptions = {
 
         try {
           // ** Login API Call to match the user credentials and receive user data in response along with his role
-          const res = await fetch(`${process.env.API_URL}/api/users/login`, {
+          const res = await fetch(`${process.env.API_URL}/api/Users/Login`, {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json'
